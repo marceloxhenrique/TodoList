00640d30c495f856248e00876a5d8652500d7466
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-10-13 23:55:42 +0200

    Add a home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,20 @@
 "use client";
-import React, { useState } from "react";
-import { Item } from "@/types/Item";
-import ListItem from "@/components/ListItem";
-import AddItem from "@/components/AddItem";
-import { v4 as uuidv4 } from "uuid";
+import React from "react";
+import { useRouter } from "next/navigation";
 
-export default function Home() {
-  const [list, setList] = useState<Item[]>([
-    { id: "1", name: "Go to the bakary", done: false },
-    { id: "2", name: "Go to supermaket", done: true },
-  ]);
-
-  const handleAddNewItem = (taskname: string) => {
-    let newList = [...list];
-    newList.push({
-      id: uuidv4(),
-      name: taskname,
-      done: false,
-    });
-    setList(newList);
-  };
-
-  const deleteTask = (task: Item) => {
-    const newList = [...list];
-    const toDelete = list.findIndex((item) => {
-      return item.id === task.id;
-    });
-    newList.splice(toDelete, 1);
-    setList(newList);
-  };
-
+const Page = () => {
+  const router = useRouter();
   return (
-    <main className="container mx-auto max-w-4xl flex min-h-screen flex-col items-center  p-10 bg-navy-blue">
-      <h1 className=" text-6xl font-bold border-2 border-transparent border-b-gray-500 w-full p-2 text-center  font-mono">
-        Todo List
-      </h1>
-
-      <section className="w-full m-2">
-        <AddItem add={handleAddNewItem} />
-
-        {list.map((item, index) => (
-          <ListItem key={index} task={deleteTask} item={item} />
-        ))}
-      </section>
-    </main>
+    <div className="h-screen w-screen flex flex-col  justify-center items-center">
+      <h1 className="m-8 text-2xl">Home Page</h1>
+      <button
+        className="py-2 px-4 bg-blue-500 rounded-md"
+        onClick={() => router.push("/tasks")}
+      >
+        Go to Tasks
+      </button>
+    </div>
   );
-}
+};
+
+export default Page;
---
+++ src/app/tasks/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+import React, { useState } from "react";
+import { Item } from "@/types/Item";
+import ListItem from "@/components/ListItem";
+import AddItem from "@/components/AddItem";
+import { v4 as uuidv4 } from "uuid";
+
+export default function Home() {
+  const [list, setList] = useState<Item[]>([]);
+
+  const handleAddNewItem = (taskname: string) => {
+    let newList = [...list];
+    newList.push({
+      id: uuidv4(),
+      name: taskname,
+      done: false,
+    });
+    setList(newList);
+  };
+
+  const deleteTask = (task: Item) => {
+    const newList = [...list];
+    const toDelete = list.findIndex((item) => {
+      return item.id === task.id;
+    });
+    newList.splice(toDelete, 1);
+    setList(newList);
+  };
+
+  return (
+    <main className="container mx-auto max-w-4xl flex min-h-screen flex-col items-center  p-10 bg-navy-blue">
+      <h1 className=" text-6xl font-bold border-2 border-transparent border-b-gray-500 w-full p-2 text-center  font-mono">
+        Todo List
+      </h1>
+
+      <section className="w-full m-2">
+        <AddItem add={handleAddNewItem} />
+
+        {list.map((item, index) => (
+          <ListItem key={index} task={deleteTask} item={item} />
+        ))}
+      </section>
+    </main>
+  );
+}
